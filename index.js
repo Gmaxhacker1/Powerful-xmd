46593e2702bece5ce7b85194f26b16a811fb6d00
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-12-03 22:33:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,20 +20,18 @@ import express from 'express';
 import chalk from 'chalk';
 
 let formatSize = sizeFormatter({
-	std: 'JEDEC',
-	decimalPlaces: '2',
-	keepTrailingZeroes: false,
-	render: (literal, symbol) => `${literal} ${symbol}B`
+  std: 'JEDEC',
+  decimalPlaces: '2',
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`
 })
-const { 
- say } = cfont
-const {
- tz } = moment
+
+const { say } = cfont
+const { tz } = moment
 const app = express();
 const port = process.env.PORT || 8079;
 const time = tz('Asia/Jakarta').format('HH:mm:ss');
 const currentFilePath = new URL(import.meta.url).pathname;
-
 
 say(info.figlet, {
   font: "simpleBlock",
@@ -41,103 +39,106 @@ say(info.figlet, {
   gradient: ["yellow", "cyan", "red"],
   transitionGradient: 1,
 })
+
 say('by ' + info.nameown, {
   font: "tiny",
   align: "center",
   colors: ["white"]
 })
+
 app.listen(port, () => {
-  console.log(chalk.green(`⚡ Port ${port} telah terbuka`));
+  console.log(chalk.green(`⚡ Port ${port} is now open`));
 });
+
 const folderPath = './tmp';
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
-        console.log(chalk.green('Folder tmp berhasil dibuat.'));
-        }
-        
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+  console.log(chalk.green('Tmp folder created successfully.'));
+}
+
 let isRunning = false;
 
 const rl = createInterface(process.stdin, process.stdout)
 
 async function start(file) {
-    if (isRunning) return;
+  if (isRunning) return;
   isRunning = true;
   const args = [join(dirname(currentFilePath), file), ...process.argv.slice(2)];
-const p = spawn(process.argv[0], args, {
+  const p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
   });
-    p.on("message", data => {
+
+  p.on("message", data => {
     console.log(chalk.magenta("[ ✅ Accepted  ]", data))
-        switch (data) {
-            case "reset":
-                p.process.kill()
-                isRunning = false
-                start.apply(this, arguments)
-                break
-            case "uptime":
-                p.send(process.uptime())
-                break
-        }
-    })
-    p.on("exit", (_, code) => {
+    switch (data) {
+      case "reset":
+        p.process.kill()
         isRunning = false
-        console.error("[❗] Keluar dengan kode :", code)
-        if (code !== 0) return start(file)
-        watchFile(args[0], () => {
-            unwatchFile(args[0])
-            start(file)
-        })
+        start.apply(this, arguments)
+        break
+      case "uptime":
+        p.send(process.uptime())
+        break
+    }
+  })
+
+  p.on("exit", (_, code) => {
+    isRunning = false
+    console.error("[❗] Exited with code:", code)
+    if (code !== 0) return start(file)
+    watchFile(args[0], () => {
+      unwatchFile(args[0])
+      start(file)
     })
-    
-    let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-    if (!opts["test"])
-        if (!rl.listenerCount()) rl.on("line", line => {
-            p.emit("message", line.trim())
-        })
+  })
 
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  if (!opts["test"])
+    if (!rl.listenerCount()) rl.on("line", line => {
+      p.emit("message", line.trim())
+    })
 
-const packageJsonPath = join(dirname(currentFilePath), './package.json');
-const pluginsFolder = join(dirname(currentFilePath), 'plugins');
-const totalFoldersAndFiles = await getTotalFoldersAndFiles(pluginsFolder);
+  const packageJsonPath = join(dirname(currentFilePath), './package.json');
+  const pluginsFolder = join(dirname(currentFilePath), 'plugins');
+  const totalFoldersAndFiles = await getTotalFoldersAndFiles(pluginsFolder);
+
   fs.readdir(pluginsFolder, async (err, files) => {
     if (err) {
-      console.error(chalk.red(`Folder Plugins Error: ${err}`));
+      console.error(chalk.red(`Plugins Folder Error: ${err}`));
       return;
     }
 
     try {
-      console.log(chalk.bgGreen(chalk.white(`Library Baileys Versi ${(await fetchLatestBaileysVersion()).version} Telah Terinstall`)));
+      console.log(chalk.bgGreen(chalk.white(`Baileys Library Version ${(await fetchLatestBaileysVersion()).version} is installed`)));
     } catch (e) {
-      console.error(chalk.bgRed(chalk.white('Library Baileys Tidak Terinstall')));
+      console.error(chalk.bgRed(chalk.white('Baileys Library is not installed')));
     }
   })
-
 
   try {
     const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8');
     const packageJsonObj = JSON.parse(packageJsonData);
-    // let fitur = Object.values(plugins).filter(v => v.help ).map(v => v.help).flat(1)
     const { data } = await axios.get('https://api.ipify.org')
     const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
     const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
 
-    console.log(`╭──⎔ Dashboard Sistem ⎔
-┣ Name Bot: ${chalk.white(packageJsonObj.name)}
-┣ Versi: ${chalk.white(packageJsonObj.version)}
-┣ Deskripsi: ${chalk.white(packageJsonObj.description)}
-┣ Os: ${chalk.white(os.type())}
-┣ Memori: ${chalk.white(freeRamInGB.toFixed(2) + ' / ' + ramInGB.toFixed(2))}
+    console.log(`╭──⎔ System Dashboard ⎔
+┣ Bot Name: ${chalk.white(packageJsonObj.name)}
+┣ Version: ${chalk.white(packageJsonObj.version)}
+┣ Description: ${chalk.white(packageJsonObj.description)}
+┣ OS: ${chalk.white(os.type())}
+┣ Memory: ${chalk.white(freeRamInGB.toFixed(2) + ' / ' + ramInGB.toFixed(2))}
 ┣ IP: ${chalk.red(data)}
 ┣ Owner: ${chalk.white(global.info.nomerown)}
 ╰──⎔⎔
-╭──⎔⎔ ${chalk.bgCyan(chalk.white('Spesifikasi Bot'))} ⎔⎔
-┣ Fitur: ${chalk.white(totalFoldersAndFiles.files)} Fitur
+╭──⎔⎔ ${chalk.bgCyan(chalk.white('Bot Specifications'))} ⎔⎔
+┣ Features: ${chalk.white(totalFoldersAndFiles.files)} Features
 ╰──⎔⎔ 
 
 ⎔──⎔⎔ Creator: ${chalk.bold.cyan('Tio')} ⎔⎔──⎔`)
-    
+
   } catch (err) {
-    console.error(chalk.red(`Tidak Bisa Membaca File package.json: ${err}`));
+    console.error(chalk.red(`Unable to read package.json file: ${err}`));
   }
 
   setInterval(() => {}, 1000);
@@ -166,6 +167,6 @@ function getTotalFoldersAndFiles(folderPath) {
 }
 
 /**
-Memulai sistem
-**/
+ * Start the system
+ */
 start('main.js');
